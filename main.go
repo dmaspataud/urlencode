4f42b71026df3d13e9c3a6b8411726d69e6212ad
dmaspataud <dorian.maspataud@gmail.com>
2021-10-26 10:20:26 +0200

    Changes to encoding function to encode space using %20 instead of +.

--- main.go
+++ main.go
@@ -39,6 +39,11 @@ func decode(encodedString string) string {
 }
 
 func encode(decodedString string) string {
-	encodedValue := url.QueryEscape(decodedString)
+	/*
+		We cheat by using url.URL instead of url.QueryEscape to get %20 instead of +.
+		then print everything but the first two chars because it's not really an url.
+	*/
+	t := &url.URL{Path: decodedString}
+	encodedValue := t.String()[2:]
 	return encodedValue
 }
